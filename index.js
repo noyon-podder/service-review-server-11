612ec7e5226f9a41fdb7c833d5b71477a2a0455c
noyon-podder <noyon.vai369@gmail.com>
2022-11-17 02:06:31 +0600

    services api is created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
@@ -12,10 +13,40 @@ app.use(cors())
 app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tlsofwm.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+      const servicesCollection = client.db('justice').collection('services');
+  
+     app.get('/homeService', async(req, res) => {
+        const query = {}
+        const result = await servicesCollection.find(query).limit(3).toArray();
+        res.send(result);
+     })
+
+
+      app.get('/services', async(req, res) => {
+          const query = {}
+          const result = await servicesCollection.find(query).toArray();
+          res.send(result);
+      })
+
+      
+    }
+    finally{
+  
+    }
+  }
+  run().catch(console.log())
+
 app.get('/', async(req, res) => {
     res.send('justice server was running')
 })
-
 
 app.listen(port, () => {
     console.log(`server running port is ${port}`)
