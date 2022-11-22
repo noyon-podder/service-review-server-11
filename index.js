4ad73a9a22d7e3b077fb9d4c359c2aaaff0e0f71
noyon-podder <noyon.vai369@gmail.com>
2022-11-22 21:03:03 +0600

    server all task is completed

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
+function verifyJwt(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized user'});
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'Forbidden Access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+  
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tlsofwm.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -79,9 +93,14 @@ async function run(){
         res.send(result); 
       })
 
-      app.get('/reviews', async(req, res) => {
+      app.get('/reviews',verifyJwt, async(req, res) => {
         const email = req.query.email;
-        console.log('User token from server', req.headers.authorization)
+
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+          return res.status(403).send({message: 'Forbidden access'})
+        }
+
         const query = {userEmail: email}
         const cursor = await reviewCollection.find(query).sort({date: -1}).toArray();
         res.send(cursor);
